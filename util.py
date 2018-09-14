a1ecff057b66ee2127c01f8fd27034bdb069517e
Manu Cornet <m@ma.nu>
2018-09-14 00:39:10 +0000

    Only show 5 most expensive paths

--- util.py
+++ util.py
@@ -191,7 +191,7 @@ def monitor_compile_progress(child_process):
     temp = [key, value]
     flattened.append(temp)
   flattened = sorted(flattened, reverse=True, key = lambda el : el[1])
-  for i in range(min(10, len(where_time_is_spent))):
+  for i in range(min(5, len(where_time_is_spent))):
     print(str(round(flattened[i][1] / 1000, 1)) + "s spent on " + flattened[i][0])
 
 def get_last_revision_number_for_cl(cl):
